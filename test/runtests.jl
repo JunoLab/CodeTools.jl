2f12e29a1f38f23156ef62931ba6adabafa59047
Mike J Innes <mike.j.innes@gmail.com>
2016-06-28 17:12:50 -0400

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,3 @@ using Base.Test, CodeTools
 @test CodeTools.getthing(Base, [:fft]) == fft
 
 @test CodeTools.filemodule(Pkg.dir("CodeTools", "src", "module.jl")) == "CodeTools"
-
-include("utils.jl")
-include("scope.jl")
-include("display.jl")
