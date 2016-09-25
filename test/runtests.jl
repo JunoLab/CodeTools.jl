242cead0885a70499e52c270c91d5c3de4a5bb51
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2016-09-25 12:20:29 +0200

    fix UndefVarError with inner functions when getting docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,12 @@ using Base.Test, CodeTools
 @test CodeTools.getthing(Base, [:fft]) == fft
 
 @test CodeTools.filemodule(Pkg.dir("CodeTools", "src", "module.jl")) == "CodeTools"
+
+let
+  function func(x)
+    infunc = (y) -> y^x
+    return infunc
+  end
+
+  @test CodeTools.hasdoc(func(3)) == false
+end
