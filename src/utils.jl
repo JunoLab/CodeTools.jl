40f3bf96d4a03ebd59551aef7419a532f072c49f
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2016-09-27 21:19:17 +0200

    add withpath from Requires.jl

--- src/utils.jl
+++ src/utils.jl
@@ -20,3 +20,17 @@ function memoize_debounce(f, expiry = 1)
     return result
   end
 end
+
+function withpath(f, path)
+  tls = task_local_storage()
+  hassource = haskey(tls, :SOURCE_PATH)
+  hassource && (path′ = tls[:SOURCE_PATH])
+  tls[:SOURCE_PATH] = path
+  try
+    return f()
+  finally
+    hassource ?
+      (tls[:SOURCE_PATH] = path′) :
+      delete!(tls, :SOURCE_PATH)
+  end
+end
