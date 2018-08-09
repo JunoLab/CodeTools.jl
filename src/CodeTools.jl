475ad206a778a809405daf3115855e0d656f766e
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-08-09 17:20:21 +0100

    update stuff for 1.0

--- src/CodeTools.jl
+++ src/CodeTools.jl
@@ -1,5 +1,3 @@
-__precompile__()
-
 module CodeTools
 
 using Lazy, LNR
@@ -8,10 +6,6 @@ include("utils.jl")
 include("eval.jl")
 include("module.jl")
 include("summaries.jl")
-
-# # Summaries shims
-# signature(_) = nothing
-# description(_) = nothing
 
 include("completions.jl")
 include("doc.jl")
